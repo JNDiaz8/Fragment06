7bc69edfbbcc21a2cbf7712904967c0e9d3fb2ea
Joaquin Diaz <nete_8_8_@hotmail.com>
2018-11-28 18:33:13 +0100

    El programa funciona correctamente. Acabo la parte de ampliando conocimientos en casa.

--- app/src/main/java/com/example/matinal/fragment06/Fragment06.java
+++ app/src/main/java/com/example/matinal/fragment06/Fragment06.java
@@ -1,13 +1,31 @@
 package com.example.matinal.fragment06;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Fragment06 extends AppCompatActivity {
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment06);
+
+        boton = findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager FM = getFragmentManager();
+                FragmentTransaction FT = FM.beginTransaction();
+                Fragmento1 fragmento1 = new Fragmento1();
+                FT.add(R.id.contenedor, fragmento1).commit();
+
+            }
+        });
     }
 }
